156a486c1553623d51663015724774b774fd24c5
Garinmckayl <natnaelgetenew@gmail.com>
2021-09-11 08:24:30 +0000

    Refactor: test

--- tests/test_kokoro.py
+++ tests/test_kokoro.py
@@ -3,10 +3,10 @@ import pytest
 from chia.types.spend_bundle import SpendBundle
 from chia.types.condition_opcodes import ConditionOpcode
 
-from localchia.localchia_drivers import (
-    create_localchia_puzzle,
-    solution_for_localchia,
-    localchia_announcement_assertion,
+from kokoro.kokoro_drivers import (
+    create_kokoro_puzzle,
+    solution_for_kokoro,
+    kokoro_announcement_assertion,
 )
 from cdv.test import setup as setup_test
 
@@ -16,20 +16,20 @@ class TestStandardTransaction:
         network, alice, bob = await setup_test()
         yield network, alice, bob
 
-    async def make_and_spend_localchia(self, network, alice, bob, CONTRIBUTION_AMOUNT):
+    async def make_and_spend_kokoro(self, network, alice, bob, CONTRIBUTION_AMOUNT):
         # Get our alice wallet some money
         await network.farm_block(farmer=alice)
 
-        # This will use one mojo to create our localchia on the blockchain.
-        localchia_coin = await alice.launch_smart_coin(create_localchia_puzzle(1000000000000, bob.puzzle_hash))
+        # This will use one mojo to create our kokoro on the blockchain.
+        kokoro_coin = await alice.launch_smart_coin(create_kokoro_puzzle(1000000000000, bob.puzzle_hash))
         # This retrieves us a coin that is at least 500 mojos.
         contribution_coin = await alice.choose_coin(CONTRIBUTION_AMOUNT)
 
-        #This is the spend of the localchia bank coin.  We use the driver code to create the solution.
-        localchia_spend = await alice.spend_coin(
-            localchia_coin,
+        #This is the spend of the kokoro bank coin.  We use the driver code to create the solution.
+        kokoro_spend = await alice.spend_coin(
+            kokoro_coin,
             pushtx=False,
-            args=solution_for_localchia(localchia_coin.as_coin(), CONTRIBUTION_AMOUNT),
+            args=solution_for_kokoro(kokoro_coin.as_coin(), CONTRIBUTION_AMOUNT),
         )
         # This is the spend of a standard coin.  We simply spend to ourselves but minus the CONTRIBUTION_AMOUNT.
         contribution_spend = await alice.spend_coin(
@@ -38,45 +38,45 @@ class TestStandardTransaction:
             amt=(contribution_coin.amount - CONTRIBUTION_AMOUNT),
             custom_conditions=[
                 [ConditionOpcode.CREATE_COIN, contribution_coin.puzzle_hash, (contribution_coin.amount - CONTRIBUTION_AMOUNT)],
-                localchia_announcement_assertion(localchia_coin.as_coin(), CONTRIBUTION_AMOUNT)
+                kokoro_announcement_assertion(kokoro_coin.as_coin(), CONTRIBUTION_AMOUNT)
             ]
         )
 
         # Aggregate them to make sure they are spent together
-        combined_spend = SpendBundle.aggregate([contribution_spend, localchia_spend])
+        combined_spend = SpendBundle.aggregate([contribution_spend, kokoro_spend])
 
         result = await network.push_tx(combined_spend)
         return result
 
     @pytest.mark.asyncio
-    async def test_localchia_contribution(self, setup):
+    async def test_kokoro_contribution(self, setup):
         network, alice, bob = setup
         try:
-            result = await self.make_and_spend_localchia(network, alice, bob, 500)
+            result = await self.make_and_spend_kokoro(network, alice, bob, 500)
 
             assert "error" not in result
 
             filtered_result = list(filter(
                 lambda addition:
                     (addition.amount == 501) and
-                    (addition.puzzle_hash == create_localchia_puzzle(1000000000000, bob.puzzle_hash).get_tree_hash())
+                    (addition.puzzle_hash == create_kokoro_puzzle(1000000000000, bob.puzzle_hash).get_tree_hash())
             ,result["additions"]))
             assert len(filtered_result) == 1
         finally:
             await network.close()
 
     @pytest.mark.asyncio
-    async def test_localchia_completion(self, setup):
+    async def test_kokoro_completion(self, setup):
         network, alice, bob = setup
         try:
-            result = await self.make_and_spend_localchia(network, alice, bob, 1000000000000)
+            result = await self.make_and_spend_kokoro(network, alice, bob, 1000000000000)
 
             assert "error" not in result
 
             filtered_result = list(filter(
                 lambda addition:
                     (addition.amount == 0) and
-                    (addition.puzzle_hash == create_localchia_puzzle(1000000000000, bob.puzzle_hash).get_tree_hash())
+                    (addition.puzzle_hash == create_kokoro_puzzle(1000000000000, bob.puzzle_hash).get_tree_hash())
             ,result["additions"]))
             assert len(filtered_result) == 1
 
@@ -90,10 +90,10 @@ class TestStandardTransaction:
             await network.close()
 
     @pytest.mark.asyncio
-    async def test_localchia_stealing(self, setup):
+    async def test_kokoro_stealing(self, setup):
         network, alice, bob = setup
         try:
-            result = await self.make_and_spend_localchia(network, alice, bob, -100)
+            result = await self.make_and_spend_kokoro(network, alice, bob, -100)
             assert 'error' in result
             assert 'GENERATOR_RUNTIME_ERROR' in result['error']
         finally:
